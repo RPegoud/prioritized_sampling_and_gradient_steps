989295f7f7e1d46c95cfa5e3fa9f936cfd14b446
Ryan Pégoud <ryanpegoud@gmail.com>
2024-04-09 17:53:48 +0200

    corrected sh script

--- src/run_base_ppo.sh
+++ src/run_base_ppo.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=ppo_base%j.err
 #SBATCH --constraint=v100-16g
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=1
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=10
 #SBATCH --time=01:00:00
@@ -14,7 +14,7 @@
 
 set -x # activer l’echo des commandes
 cd $WORK/prioritized_sampling_and_gradient_steps/src/
-srun python -u python base_ppo.py --total-timesteps 50000 --log-results
+srun python -u base_ppo.py --total-timesteps 50000 --log-results
 
-# srun --job-name=ppo_base --output=ppo_base%j.out --error=ppo_base%j.err --constraint=v100-16g --nodes=1 --ntasks=4 --gres=gpu:4 --cpus-per-task=10 --time=01:00:00 --qos=qos_gpu-dev --hint=nomultithread --account=xyk@v100 --pty bash
+# srun --job-name=ppo_base --output=ppo_base%j.out --error=ppo_base%j.err --constraint=v100-16g --nodes=1 --ntasks=1 --gres=gpu:4 --cpus-per-task=10 --time=01:00:00 --qos=qos_gpu-dev --hint=nomultithread --account=xyk@v100 --pty bash
 # python -u python base_ppo.py --total-timesteps 50000 --log-results
