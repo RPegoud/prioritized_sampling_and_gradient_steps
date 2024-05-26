ce375d80f03871460c0ef07d65136e036dc975f3
Ryan Pégoud <ryanpegoud@gmail.com>
2024-05-26 21:43:09 +0200

    added low alpha discrete script

---
+++ src/run_all_experiments_low_alpha.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <environment-name>"
+    exit 1
+fi
+
+env=$1
+
+trainers=(
+    "parallel_ppo_1c"
+    "parallel_ppo_1d"
+)
+
+alphas=("0.05" "0.1")
+
+function create_and_submit {
+    trainer=$1
+    env=$2
+    alpha=$3
+
+    sbatch <<EOF
+#!/bin/bash
+#SBATCH --job-name=${trainer}_${env}_${alpha}
+#SBATCH --output=${trainer}_${env}_${alpha}%j.out
+#SBATCH --error=${trainer}_${env}_${alpha}%j.err
+#SBATCH --constraint=v100-16g
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=10
+#SBATCH --time=02:00:00
+#SBATCH --qos=qos_gpu-dev
+#SBATCH --hint=nomultithread
+#SBATCH --account=xyk@v100
+
+module purge
+conda deactivate
+
+set -x
+module load python/3.10.4
+conda activate igs
+wandb offline
+cd \$WORK/prioritized_sampling_and_gradient_steps/src
+python main.py --total-timesteps 5000000 --log-results --trainer "$trainer" --env-name "$env" --alpha "$alpha"
+EOF
+
+    echo "Submitting ${trainer} run on ${env}, alpha: ${alpha}"
+}
+
+for trainer in "${trainers[@]}"; do
+    if [[ "$trainer" == "parallel_ppo_1c" || "$trainer" == "parallel_ppo_1d" ]]; then
+        for alpha in "${alphas[@]}"; do
+            create_and_submit "$trainer" "$env" "$alpha"
+        done
+    else
+        create_and_submit "$trainer" "$env" "0.2"
+    fi
+done
